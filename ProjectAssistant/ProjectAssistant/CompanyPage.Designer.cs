66b895d0793c7e182af1eb58cfa088d6efb4efc2
ulasa <ulasa@LAPTOP-R8BU2B2A>
2018-04-14 19:24:10 +0300

    kjhkjhk

--- ProjectAssistant/ProjectAssistant/CompanyPage.Designer.cs
+++ ProjectAssistant/ProjectAssistant/CompanyPage.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabProject = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabProject.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(227, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // tabProject
+            // 
+            this.tabProject.Controls.Add(this.tabPage1);
+            this.tabProject.Controls.Add(this.tabPage2);
+            this.tabProject.Location = new System.Drawing.Point(3, 3);
+            this.tabProject.Name = "tabProject";
+            this.tabProject.SelectedIndex = 0;
+            this.tabProject.Size = new System.Drawing.Size(586, 352);
+            this.tabProject.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(578, 326);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Projects";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(578, 326);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "New Project";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // CompanyPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tabProject);
+            this.Controls.Add(this.label3);
             this.Name = "CompanyPage";
-            this.Size = new System.Drawing.Size(674, 468);
+            this.Size = new System.Drawing.Size(1640, 960);
+            this.Load += new System.EventHandler(this.CompanyPage_Load);
+            this.tabProject.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabControl tabProject;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
