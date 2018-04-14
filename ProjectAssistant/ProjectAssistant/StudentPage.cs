e09880ecf22f7ea23d0cd789c02513eb48cca66e
Berk Karaağaç <animant@hotmail.com>
2018-04-15 01:14:02 +0300

    student page düzenlendi

--- ProjectAssistant/ProjectAssistant/StudentPage.cs
+++ ProjectAssistant/ProjectAssistant/StudentPage.cs
@@ -14,8 +14,10 @@ namespace ProjectAssistant
     {
         public StudentPage()
         {
-
             InitializeComponent();
+
+
+            
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -41,6 +43,32 @@ namespace ProjectAssistant
         private void StudentPage_Load(object sender, EventArgs e)
         {
             
+        }
+
+        private void textBox_studPhoneNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        
+
+        
+
+        private void richTextBox_studEducation_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip tltip = new ToolTip();
+            tltip.SetToolTip(this.richTextBox_studEducation, "Kadıköy Anatolian High School (2010-2014)");
+            
+        }
+
+        private void richTextBox_studWork_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip tltip = new ToolTip();
+            tltip.SetToolTip(this.richTextBox_studWork, "Intern at Mercedes Benz Türk A.Ş (07.2015-08.2015)");
         }
     }
 }
