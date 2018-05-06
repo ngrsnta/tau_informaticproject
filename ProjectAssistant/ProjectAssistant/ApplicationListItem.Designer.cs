6401a50385d650c62dc9193114caf772124793dd
Berk Karaağaç <animant@hotmail.com>
2018-05-06 13:18:20 +0300

    Application list item added

---
+++ ProjectAssistant/ProjectAssistant/ApplicationListItem.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace ProjectAssistant
+{
+    partial class ApplicationListItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_dontTouch = new System.Windows.Forms.Label();
+            this.label_projectEnd = new System.Windows.Forms.Label();
+            this.label_projectStart = new System.Windows.Forms.Label();
+            this.label_projectDurationSign = new System.Windows.Forms.Label();
+            this.richTextBox_companyName = new System.Windows.Forms.RichTextBox();
+            this.label_projectRemainingDays = new System.Windows.Forms.Label();
+            this.richTextBox_projectDeadline = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_description = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_projectArea = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_projectTitle = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_applyStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label_dontTouch
+            // 
+            this.label_dontTouch.AutoSize = true;
+            this.label_dontTouch.Enabled = false;
+            this.label_dontTouch.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_dontTouch.Location = new System.Drawing.Point(681, 129);
+            this.label_dontTouch.Name = "label_dontTouch";
+            this.label_dontTouch.Size = new System.Drawing.Size(12, 17);
+            this.label_dontTouch.TabIndex = 19;
+            this.label_dontTouch.Text = "-";
+            // 
+            // label_projectEnd
+            // 
+            this.label_projectEnd.AutoSize = true;
+            this.label_projectEnd.Enabled = false;
+            this.label_projectEnd.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_projectEnd.Location = new System.Drawing.Point(697, 129);
+            this.label_projectEnd.Name = "label_projectEnd";
+            this.label_projectEnd.Size = new System.Drawing.Size(72, 17);
+            this.label_projectEnd.TabIndex = 18;
+            this.label_projectEnd.Text = "20.15.2017";
+            // 
+            // label_projectStart
+            // 
+            this.label_projectStart.AutoSize = true;
+            this.label_projectStart.Enabled = false;
+            this.label_projectStart.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_projectStart.Location = new System.Drawing.Point(614, 129);
+            this.label_projectStart.Name = "label_projectStart";
+            this.label_projectStart.Size = new System.Drawing.Size(72, 17);
+            this.label_projectStart.TabIndex = 17;
+            this.label_projectStart.Text = "15.15.2017";
+            // 
+            // label_projectDurationSign
+            // 
+            this.label_projectDurationSign.AutoSize = true;
+            this.label_projectDurationSign.Enabled = false;
+            this.label_projectDurationSign.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_projectDurationSign.Location = new System.Drawing.Point(644, 113);
+            this.label_projectDurationSign.Name = "label_projectDurationSign";
+            this.label_projectDurationSign.Size = new System.Drawing.Size(94, 16);
+            this.label_projectDurationSign.TabIndex = 16;
+            this.label_projectDurationSign.Text = "Project Duration";
+            // 
+            // richTextBox_companyName
+            // 
+            this.richTextBox_companyName.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox_companyName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox_companyName.Enabled = false;
+            this.richTextBox_companyName.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox_companyName.Location = new System.Drawing.Point(327, 37);
+            this.richTextBox_companyName.Name = "richTextBox_companyName";
+            this.richTextBox_companyName.ReadOnly = true;
+            this.richTextBox_companyName.Size = new System.Drawing.Size(234, 92);
+            this.richTextBox_companyName.TabIndex = 15;
+            this.richTextBox_companyName.Text = "COMPANY NAME BIG";
+            // 
+            // label_projectRemainingDays
+            // 
+            this.label_projectRemainingDays.AutoSize = true;
+            this.label_projectRemainingDays.Enabled = false;
+            this.label_projectRemainingDays.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_projectRemainingDays.Location = new System.Drawing.Point(644, 25);
+            this.label_projectRemainingDays.Name = "label_projectRemainingDays";
+            this.label_projectRemainingDays.Size = new System.Drawing.Size(111, 16);
+            this.label_projectRemainingDays.TabIndex = 14;
+            this.label_projectRemainingDays.Text = "XX Days Remaining";
+            // 
+            // richTextBox_projectDeadline
+            // 
+            this.richTextBox_projectDeadline.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox_projectDeadline.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox_projectDeadline.Enabled = false;
+            this.richTextBox_projectDeadline.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox_projectDeadline.Location = new System.Drawing.Point(658, 3);
+            this.richTextBox_projectDeadline.Name = "richTextBox_projectDeadline";
+            this.richTextBox_projectDeadline.Size = new System.Drawing.Size(111, 25);
+            this.richTextBox_projectDeadline.TabIndex = 13;
+            this.richTextBox_projectDeadline.Text = "DEADLINE";
+            // 
+            // richTextBox_description
+            // 
+            this.richTextBox_description.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox_description.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox_description.Enabled = false;
+            this.richTextBox_description.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox_description.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.richTextBox_description.Location = new System.Drawing.Point(9, 77);
+            this.richTextBox_description.Name = "richTextBox_description";
+            this.richTextBox_description.Size = new System.Drawing.Size(312, 73);
+            this.richTextBox_description.TabIndex = 12;
+            this.richTextBox_description.Text = "Description here";
+            // 
+            // richTextBox_projectArea
+            // 
+            this.richTextBox_projectArea.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox_projectArea.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox_projectArea.Enabled = false;
+            this.richTextBox_projectArea.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox_projectArea.Location = new System.Drawing.Point(9, 46);
+            this.richTextBox_projectArea.Name = "richTextBox_projectArea";
+            this.richTextBox_projectArea.Size = new System.Drawing.Size(292, 25);
+            this.richTextBox_projectArea.TabIndex = 11;
+            this.richTextBox_projectArea.Text = "Area of Interest here";
+            // 
+            // richTextBox_projectTitle
+            // 
+            this.richTextBox_projectTitle.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox_projectTitle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox_projectTitle.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.richTextBox_projectTitle.Enabled = false;
+            this.richTextBox_projectTitle.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox_projectTitle.Location = new System.Drawing.Point(9, 15);
+            this.richTextBox_projectTitle.Name = "richTextBox_projectTitle";
+            this.richTextBox_projectTitle.ReadOnly = true;
+            this.richTextBox_projectTitle.Size = new System.Drawing.Size(292, 24);
+            this.richTextBox_projectTitle.TabIndex = 10;
+            this.richTextBox_projectTitle.Text = "Project Title here";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(613, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 21);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Application Status";
+            // 
+            // textBox_applyStatus
+            // 
+            this.textBox_applyStatus.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox_applyStatus.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_applyStatus.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_applyStatus.Location = new System.Drawing.Point(647, 77);
+            this.textBox_applyStatus.Name = "textBox_applyStatus";
+            this.textBox_applyStatus.Size = new System.Drawing.Size(100, 16);
+            this.textBox_applyStatus.TabIndex = 21;
+            this.textBox_applyStatus.Text = "Submitted";
+            // 
+            // ApplicationListItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox_applyStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_dontTouch);
+            this.Controls.Add(this.label_projectEnd);
+            this.Controls.Add(this.label_projectStart);
+            this.Controls.Add(this.label_projectDurationSign);
+            this.Controls.Add(this.richTextBox_companyName);
+            this.Controls.Add(this.label_projectRemainingDays);
+            this.Controls.Add(this.richTextBox_projectDeadline);
+            this.Controls.Add(this.richTextBox_description);
+            this.Controls.Add(this.richTextBox_projectArea);
+            this.Controls.Add(this.richTextBox_projectTitle);
+            this.Name = "ApplicationListItem";
+            this.Size = new System.Drawing.Size(779, 153);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_dontTouch;
+        private System.Windows.Forms.Label label_projectEnd;
+        private System.Windows.Forms.Label label_projectStart;
+        private System.Windows.Forms.Label label_projectDurationSign;
+        private System.Windows.Forms.RichTextBox richTextBox_companyName;
+        private System.Windows.Forms.Label label_projectRemainingDays;
+        private System.Windows.Forms.RichTextBox richTextBox_projectDeadline;
+        private System.Windows.Forms.RichTextBox richTextBox_description;
+        private System.Windows.Forms.RichTextBox richTextBox_projectArea;
+        private System.Windows.Forms.RichTextBox richTextBox_projectTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_applyStatus;
+    }
+}
